f782d97ba9d4c45625de3a1a4d1d08e9bc3023ee
Jaco du Plessis <jaco@jacoduplessis.co.za>
2018-12-01 22:59:25 +0200

    Clean

--- get_input.py
+++ get_input.py
@@ -6,7 +6,6 @@ from urllib.request import urlopen, Request
 year = sys.argv[1]
 days = sys.argv[2:]
 
-
 if not all([year, days]):
     print("Invalid input. Usage: python3 get_input.py <YEAR> <DAYS>")
     sys.exit(1)
@@ -15,9 +14,8 @@ os.makedirs(year, exist_ok=True)
 session = pathlib.Path('session.txt').read_text()
 
 for day in days:
-
     req = Request(f'https://adventofcode.com/{year}/day/{day}/input', headers={
-        'Cookie':f'session={session}'
+        'Cookie': f'session={session}'
     })
 
     resp = urlopen(req).read()
@@ -26,4 +24,3 @@ for day in days:
 
     with open(os.path.join(year, f'{to}.txt'), 'wb') as out:
         out.write(resp)
-
